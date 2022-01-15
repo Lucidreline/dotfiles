0d5c8be4f9f101c564c99ba619ba4eb8e4c0af1a
Lucidreline <casta.ma502@gmail.com>
2022-01-15 07:20:19 +0000

    Added basic fish and vim config files.

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	set fish_greeting
+	neofetch
+	echo ""
+	echo "oh manny... i see you're back again... great..."
+	
+	alias vim "nvim"
+	alias g "git"
+	alias ga "git add ."
+end
