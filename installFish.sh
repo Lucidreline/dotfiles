0164bf92f6c9c1b66f021b5ae4a29e2d2860531c
Lucidreline <casta.ma502@gmail.com>
2022-01-21 04:41:41 +0000

    Updated setup.sh.

--- installFish.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt update
-sudo apt install fish
-
-chsh -s /usr/bin/fish
--- setup.sh
+++ setup.sh
@@ -23,7 +23,9 @@ cp .zshrc ~/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions # auto suggestions
 
 git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten # theme
+
 ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+
 ln -s "$ZSH_CUSTOM/themes/typewritten/async.zsh" "$ZSH_CUSTOM/themes/async"
 
 # install btm (view cpu, ram, etc usage)
