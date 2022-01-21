fdef4f9000adf0de112dc7bc80badf61c04fd0dc
Lucidreline <casta.ma502@gmail.com>
2022-01-21 02:37:32 +0000

    Switched to zsh.

---
+++ setup.sh
@@ -0,0 +1,52 @@
+# updating
+sudo apt update -y
+sudo apt upgrade -y
+
+# install apt packages
+sudo apt install neovim tmux neofetch git default-jre curl -y 
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# oh my zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions # auto suggestions
+
+git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten # theme
+ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+ln -s "$ZSH_CUSTOM/themes/typewritten/async.zsh" "$ZSH_CUSTOM/themes/async"
+
+# install btm (view cpu, ram, etc usage)
+curl -LO https://github.com/ClementTsang/bottom/releases/download/0.6.6/bottom_0.6.6_amd64.deb
+sudo dpkg -i bottom_0.6.6_amd64.deb
+sudo rm bottom_0.6.6_amd64.deb
+
+# docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update -y
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+sudo chmod 666 /var/run/docker.sock
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# copy dot files
+cat .config/fish/config.fish > ../.config/fish/config.fish
+cat .config/nvim/init.vim > ../.config/nvim/init.vim
+
+
+# git config
+git config --global user.email "casta.ma502@gmail.com"
+git config --global user.name "Lucidreline"
+
